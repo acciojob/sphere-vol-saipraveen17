9908c55054245c31c89244b8f80aac086f37eb13
Praveen Katikireddy <praveenk9417@gmail.com>
2023-04-14 16:17:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function volume_sphere() {
-    //Write your code here
-  
+    //Write your code her
+	const vol = document.getElementById('volume');
+	const radius = document.getElementById('radius').value;
+	vol.value = ((4/3)*(22/7))*(Math.pow(radius, 3));
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
