71d4d11e9f2ffbadb4cd80f0273cf1e227da978d
Praveen Katikireddy <praveenk9417@gmail.com>
2023-04-15 14:30:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function volume_sphere(event) {
   event.preventDefault(); // prevent form submission behavior
   const radius = document.getElementById("radius").value;
   const volume = (4/3) * Math.PI * Math.pow(radius, 3);
-  document.getElementById("volume").value = volume.toFixed(2);
+  document.getElementById("volume").value = volume.toFixed(4);
 } 
 
 window.onload = function() {
